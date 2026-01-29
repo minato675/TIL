5230d5944305447f8835946584400252de1c6704
minato675 <minato675@gmail.com>
2026-01-29 17:46:43 +0900

    0129

---
+++ baekjoon/0126/24262.py
@@ -0,0 +1,5 @@
+def MenOfPassion(A[], n) 
+    i = ⌊n / 2⌋;
+    return A[i]; # 코드1
+
+MenOfPassion(,1)
---
+++ gpt_python/day17/1.py
@@ -0,0 +1,6 @@
+# 다음 코드의 오류를 찾고 수정하세요
+def add(a, b):
+    print(a + b)
+
+result = add(3, 4)
+print(result + 1)
